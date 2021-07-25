b3362777fc4d81b1e81c3de1333d7ccf8a293c55
Pragnesh_sharma <pragnesh.akshar@gmail.com>
2021-07-25 22:03:21 +0530

    add the files

--- ERPApp/src/environments/environment.prod.ts
+++ ERPApp/src/environments/environment.prod.ts
@@ -1,5 +1,23 @@
+////// How to Implement new environment
+// https://codinglatte.com/posts/angular/environment-variables-angular/
+// ng build --configuration=beta
 export const environment = {
-  production: true,
+  production: window["env"]["production"] || true,
+  timeSpanMedium: window["env"]["timeSpanMedium"] || 5000,
+  timeSpanLarge: window["env"]["timeSpanLarge"] || 7000,
+  pageSize: window["env"]["pageSize"] || 20,
+  GoogleClientId: window["env"]["GoogleClientId"] || "981659961080-9qeq7ra4kt6k47t9k1lnfbvrjrglom5l.apps.googleusercontent.com",
+  FacebookClientId: window["env"]["FacebookClientId"] || "227118115749667",
+  Timeout: window["env"]["Timeout"] || 60,
+  ShowConsoleLogs: window["env"]["ShowConsoleLogs"] || false,
+  ShowTimerLogs: window["env"]["ShowTimerLogs"] || false,
+  apiURL: window["env"]["apiURL"] || 'https://uninoter.softinform.dk/api',
+  webshipperapiURL: 'https://milcom.api.webshipper.io/v2',
+  // apiURL: 'https://test2.marketsoft.info/api'
+  // apiURL: 'https://uninoterapi.salesoptimizer.eu'
+  // apiURL: 'http://10.0.0.103:8164'
   clientId: 'ed4d96ad-bec0-4394-9616-c965ef651405',
   redirectUri: 'https://test-pssplerp-webapp.azurewebsites.net/'
 };
+////// For Set the Variable Values
+//https://pumpingco.de/blog/environment-variables-angular-docker/
--- ERPApp/src/environments/environment.ts
+++ ERPApp/src/environments/environment.ts
@@ -1,12 +1,31 @@
 // This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+////// How to Implement new environment
+// https://codinglatte.com/posts/angular/environment-variables-angular/
+// ng build --configuration=beta
 export const environment = {
   production: false,
-  clientId: 'c8a20593-c899-450e-aba9-dde0107d355c',
+  timeSpanMedium: 5000,
+  timeSpanLarge: 7000,
+  pageSize: 20,
+  GoogleClientId: "981659961080-9qeq7ra4kt6k47t9k1lnfbvrjrglom5l.apps.googleusercontent.com",
+  FacebookClientId: "1046651368784150",
+  Timeout: 60,
+  ShowConsoleLogs: false,
+  ShowTimerLogs: false,
+  // apiURL: 'https://uninoter.salesoptimizer.eu/api'
+  apiURL: 'https://uninoter.salesoptimizer.eu/api',
+  webshipperapiURL: 'https://milcom.api.webshipper.io/v2',
+  webshipperDotNetapiURL: 'http://localhost:64121',
+  // apiURL: 'http://10.0.0.103:8164'
+  // apiURL: 'http://localhost:52947/api'
+ clientId: 'c8a20593-c899-450e-aba9-dde0107d355c',
   redirectUri: 'http://localhost:4200'
 };
+////// For Set the Variable Values
+//https://pumpingco.de/blog/environment-variables-angular-docker/
 
 /*
  * For easier debugging in development mode, you can import the following file
@@ -15,4 +34,4 @@ export const environment = {
  * This import should be commented out in production mode because it will have a negative impact
  * on performance if an error is thrown.
  */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
