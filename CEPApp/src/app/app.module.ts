438ff9cf263aa51fdc4e9130215acd290441cc1f
Pragnesh_sharma <pragnesh.akshar@gmail.com>
2021-08-19 22:40:25 +0530

    add the reference in app.module.ts

--- CEPApp/src/app/app.module.ts
+++ CEPApp/src/app/app.module.ts
@@ -1,8 +1,27 @@
-import { NgModule } from '@angular/core';
+//Basic Modules
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+
+//User Defined Modules
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule, HTTP_INTERCEPTORS, HttpClient } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+// import { AuthInterceptor } from './Components/Admin/auth.interceptor';
+import { MatDialogModule } from '@angular/material/dialog';//For Popup window
+import { ToastrModule } from 'ngx-toastr';//For show alert poup message
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';//For Translation
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';//For Translation
+import { NgxPaginationModule } from 'ngx-pagination';//For Pagination
+import { Ng2OrderModule } from 'ng2-order-pipe';//For Sorting
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';//For Datepicker
+
 import { AppComponent } from './app.component';
+
+//For Translation
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -10,6 +29,28 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    // SharedModule,//For those component need for multiple modules
+    BrowserAnimationsModule,//For Popup & Toaster
+    MatDialogModule,//For Popup
+    ToastrModule.forRoot({ //For Toaster
+      progressBar: true
+    }),
+    HttpClientModule,//For HTTP call
+    FormsModule,//For Template driven forms
+    ReactiveFormsModule,//For Reactive forms
+    // AdminModule, //For Admin Defined Modules
+    // UserModule, //For User Defined Modules
+    TranslateModule.forRoot({//For Translation
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    NgxPaginationModule,//For Pagination
+    Ng2OrderModule,//For Sorting   
+    BsDatepickerModule.forRoot(),//For Datepicker
+    // NumberDirectiveModule,
     AppRoutingModule
   ],
   providers: [],
